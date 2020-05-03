50478c2c781782c7bea2739e0d19059484368693
unknown <ninahb746@gmail.com user.name NinahMo>
2020-05-03 19:21:04 +0300

    Added business logic to the application

---
+++ Documents/Akan-Name-Generator/js/scripts.js
@@ -0,0 +1,4 @@
+//business logic
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "KWaku", "Yaw", "Kofi", "Kwame"];
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Yaa"];
