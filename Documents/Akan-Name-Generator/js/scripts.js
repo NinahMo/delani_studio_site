bb8caf792f693ce09626a428a84d6cba5c4f54c1
unknown <ninahb746@gmail.com user.name NinahMo>
2020-05-03 19:30:29 +0300

    added user-interface to the application

--- Documents/Akan-Name-Generator/js/scripts.js
+++ Documents/Akan-Name-Generator/js/scripts.js
@@ -2,3 +2,13 @@
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "KWaku", "Yaw", "Kofi", "Kwame"];
 var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Yaa"];
+
+
+//user-interface logic
+function(getNames){
+  var day = document.getElementById("day").value;
+  var month = document.getElementByid("month").value;
+  var year = document.getElementById("year").value;
+  var male = document.getElementById("male").value;
+  var female = document.getElementById("female").value;
+}
