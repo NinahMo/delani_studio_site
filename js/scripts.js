6d144b31c604eb0faa76d2662b64e331f59b2a1b
unknown <ninahb746@gmail.com user.name NinahMo>
2020-05-18 15:25:03 +0300

    Initialize the directory

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$('.main').click(function () {
+    $(this).children('.hide').slideToggle(1200);
+    $(this).children('.show').toggle(1500);
+  });
+
+$(".card").hover(function () {
+    $(this).children(".card-1").fadeToggle(1000, "linear");
+});
+
+$("button").click(function (ninah) {
+    var client = document.getElementById('nameDetail').value;
+    alert(' Dear ' + 'customer' + ' your message has been received successfully.')
+    ninah.preventDefault();
+  });
+  
